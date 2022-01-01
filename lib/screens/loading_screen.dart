f3aef4345e479587b2b38b5be5d34cf0638b0469
marianneleal <marianneleal93@gmail.com>
2022-01-01 14:03:32 +0100

    added await keyword

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -24,7 +24,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getLocationData() async {
     WeatherModel weatherModel = new WeatherModel();
-    var weatherData = weatherModel.getLocationWeather();
+    var weatherData = await weatherModel.getLocationWeather();
 
     Navigator.push(context, MaterialPageRoute(builder: (context) {
       return LocationScreen(locationWeather: weatherData);
