6a98fc557e362378675181ab8399f4704044f80e
marianneleal <marianneleal93@gmail.com>
2022-01-01 15:23:56 +0100

    changed temperatures to fahrenheit

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -3,27 +3,27 @@ import 'package:clima/services/networking.dart';
 
 const openWeatherMapURL = 'https://samples.openweathermap.org/data/2.5/weather';
 
-Future<dynamic> getCityWeather(String cityName) async {
-  var url =
-      '$openWeatherMapURL?q=$cityName&&appid=b6907d289e10d714a6e88b30761fae22#&units=metric';
-  NetworkHelper networkHelper = NetworkHelper(url);
-
-  var weatherData = await networkHelper.getData();
-  return weatherData;
-}
-
-Future<dynamic> getLocationWeather() async {
-  Location loc = Location();
-  await loc.getCurrentLocation();
-
-  NetworkHelper helper = NetworkHelper(
-      '$openWeatherMapURL?lat=${loc.latitude}&lon=${loc.longitude}&&appid=b6907d289e10d714a6e88b30761fae22#&units=metric');
-
-  var weatherData = await helper.getData();
-  return weatherData;
-}
-
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    var url =
+        '$openWeatherMapURL?q=$cityName&&appid=b6907d289e10d714a6e88b30761fae22#&units=metric';
+    NetworkHelper networkHelper = NetworkHelper(url);
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location loc = Location();
+    await loc.getCurrentLocation();
+
+    NetworkHelper helper = NetworkHelper(
+        '$openWeatherMapURL?lat=${loc.latitude}&lon=${loc.longitude}&&appid=b6907d289e10d714a6e88b30761fae22#&units=metric');
+
+    var weatherData = await helper.getData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
@@ -45,11 +45,11 @@ class WeatherModel {
   }
 
   String getMessage(int temp) {
-    if (temp > 25) {
+    if (temp > 77) {
       return 'It\'s 🍦 time';
-    } else if (temp > 20) {
+    } else if (temp > 68) {
       return 'Time for shorts and 👕';
-    } else if (temp < 10) {
+    } else if (temp < 50) {
       return 'You\'ll need 🧣 and 🧤';
     } else {
       return 'Bring a 🧥 just in case';
